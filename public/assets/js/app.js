1f4630206e3fd00a526cb8fbd1f6f508c7914eb5
Chris <chrisgoerler@gmail.com>
2018-07-21 23:24:13 -0400

    fixed modal title issue displaying title on saved button

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,6 @@
 $(".preview").on('click', function (event) {
     let link = {"link": $(this).attr('data')};
-    $('.modal-title').html($(this).attr('data-title'))
+    $('.modal-title1').html($(this).attr('data-title'))
     $.post("/", link, function (data) {
         $('#modal-summary').html(data);
 
